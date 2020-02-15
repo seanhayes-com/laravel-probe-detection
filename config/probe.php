a32ff010830d2755925ed80672af22b307a4f465
Sean Hayes <sean@metalsoftware.com>
2020-02-15 12:26:03 -0500

    0.1.1

--- config/probe.php
+++ config/probe.php
@@ -11,15 +11,86 @@ return [
      * Determine if we should look up the GEO Location
      */
     'geolocate_ip' => true,
-
+	
     /*
-     * Here you may configure the "store" that the underlying Client will
-     * use to store it's data.  You may also add extra parameters that will
-     * be passed on setCacheConfig.
-     *
-     * Optional parameters: "lifetime", "prefix"
+     * IP addresses to ignore logging for
      */
-    'cache' => [
-        'store' => 'file',
-    ],
+    'ignore_ips' => array(
+    	'127.0.0.2',
+    ),
+	
+    /*
+     * User Agents to ignore logging
+     */
+    'ignore_agents' => array(
+    	'Googlebot',
+		'Bingbot',
+		'Slurp',
+		'DuckDuckBot',
+		'Baiduspider',
+		'YandexBot',
+		'Sogou',
+		'Exabot',
+		'facebookexternalhit',
+		'facebot',
+		'ia_archiver'
+    ),
+	
+    /*
+     * ISO Country codes to ignore logging
+     */
+    'ignore_isocodes' => array(
+    	
+    ),
+	
+    /*
+     * IP addresses to auto ban
+     */
+    'watch_ips' => array(
+  
+    ),
+	
+    /*
+     * User Agents to auto ban
+	 * See http://www.botreports.com/badbots/index.shtml
+     */
+    'watch_agents' => array(
+    	'80legs.com',
+		'Aboundex',
+		'BLEXBot',
+		'BlinkaCrawler',
+		'BomboraBot',
+		'BUbiNG',
+		'Bytespider',
+		'CCBot',
+		'CRAZYWEBCRAWLER',
+		'DotBot',
+		'EventMachine',
+		'istellabot',
+		'libwww-perl',
+		'LMAO',
+		'MauiBot',
+		'MJ12bot',
+		'NerdyBot',
+		'PhantomJS'
+    ),
+	
+    /*
+     * ISO Country codes to auto ban (Alpha 2-codes)
+	 * https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
+     */
+    'watch_isocodes' => array(
+    	
+    ),
+	
+    /*
+     * Referrers to auto ban
+     */
+    'watch_refers' => array(
+  	  	'.cn',
+		'.ru',
+    ),
+	
+	
+
 ];
