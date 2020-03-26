f2486a5c70a56227b08c4808239df6ec500b90be
Sean Hayes <sean@metalsoftware.com>
2020-03-26 15:45:47 -0400

    Add unit tests, code cleanup

--- src/Probe.php
+++ src/Probe.php
@@ -7,20 +7,31 @@ use Torann\GeoIP\Facades\GeoIP;
 
 class Probe
 {
+	
+	 
+	public function __construct($identifier = 'default', $do_geolocate = false, $do_hostname = false) {
+		$this->target_ip = $this->setTargetIp();
+		$this->target_agent = $this->setUserAgent();
+		$this->identifier = $this->setIndentifier($identifier);
+		$this->request_uri = $this->setRequestUri();
+		$this->request_refer = $this->setRequestRefer();
+		$this->request_host = $this->setRequestHost();
+		$this->target_location = ($do_geolocate) ? geoip($this->target_ip) : '';
+		$this->target_city = $this->setTargetCity($this->target_location);
+		$this->target_state = $this->setTargetState($this->target_location);
+		$this->target_postal_code = $this->setTargetPostalCode($this->target_location);
+		$this->target_hostname = ($do_hostname) ? gethostbyaddr($this->target_ip) : '';
+		$this->target_iso_code = $this->setTargetIsoCode($this->target_location);
+	}
 
     public static function logRequest($identifier = 'default')
     {
 
-        //dd(config('probe'));
         $do_geolocate = config('probe.geolocate_ip');
         $do_hostname = config('probe.resolve_hostnames');
-
         $ignore_ips = config('probe.ignore_ips');
         $ignore_agents = config('probe.ignore_agents');
         $ignore_isocodes = config('probe.ignore_isocodes');
-
-        //dd($ignore_agents);
-
         $watch_ips = config('probe.watch_ips');
         $watch_agents = config('probe.watch_agents');
         $watch_isocodes = config('probe.watch_isocodes');
@@ -30,27 +41,11 @@ class Probe
         $dolog = false;
         $doban = false;
 
-        $target = [];
-
-        $target['ip'] = (isset($_SERVER['REMOTE_ADDR'])) ? $_SERVER['REMOTE_ADDR'] : '127.0.0.1';
-        $target_location = ($do_geolocate) ? geoip($target['ip']) : '';
-        $target['agent'] = (isset($_SERVER['HTTP_USER_AGENT'])) ? $_SERVER['HTTP_USER_AGENT'] : '';
-
-        $target['identifier'] = (!empty($identifier)) ? $identifier : '';
-        $target['uri'] = (isset($_SERVER['REQUEST_URI'])) ? $_SERVER['REQUEST_URI'] : '';
-        $target['refer'] = (isset($_SERVER['HTTP_REFERER'])) ? $_SERVER['HTTP_REFERER'] : '';
-        $target['host'] = (isset($_SERVER['HTTP_HOST'])) ? $_SERVER['HTTP_HOST'] : '';
-        $target['city'] = (isset($target_location->city) && !empty($target_location->city)) ? $target_location->city : '';
-        $target['state'] = (isset($target_location->state) && !empty($target_location->state)) ? $target_location->state : '';
-        $target['postcode'] = (isset($target_location->postal_code) && !empty($target_location->postal_code)) ? $target_location->postal_code : '';
-        $target['hostname'] = ($do_hostname) ? gethostbyaddr($target['ip']) : '';
-        $target['created_at'] = date('Y-m-d H:i:s');
-        $target['updated_at'] = date('Y-m-d H:i:s');
-        $target['iso_code'] = (isset($target_location->iso_code) && !empty($target_location->iso_code)) ? $target_location->iso_code : '';
+		$target = $this->setTarget();
 		
-        $ignore_this_ip = self::exactInList($target['ip'], $ignore_ips);
-        $ignore_this_agent = self::matchInList($target['agent'], $ignore_agents);
-        $ignore_this_isocode = self::exactInList($target['iso_code'], $ignore_isocodes);
+        $ignore_this_ip = $this->exactInList($target['ip'], $ignore_ips);
+        $ignore_this_agent = $this->matchInList($target['agent'], $ignore_agents);
+        $ignore_this_isocode = $this->exactInList($target['iso_code'], $ignore_isocodes);
 
         if ($ignore_this_ip || $ignore_this_agent || $ignore_this_isocode) {
             $dolog = false;
@@ -59,11 +54,11 @@ class Probe
             $dolog = true;
         }
 
-        $watch_this_ip = self::exactInList($target['ip'], $watch_ips);
-        $watch_this_agent = self::matchInList($target['agent'], $watch_agents);
-        $watch_this_isocode = self::exactInList($target['iso_code'], $watch_isocodes);
-        $watch_this_refer = self::matchInList($target['refer'], $watch_refers);
-		$watch_this_uri = self::matchInList($target['uri'], $watch_uris);
+        $watch_this_ip = $this->exactInList($target['ip'], $watch_ips);
+        $watch_this_agent = $this->matchInList($target['agent'], $watch_agents);
+        $watch_this_isocode = $this->exactInList($target['iso_code'], $watch_isocodes);
+        $watch_this_refer = $this->matchInList($target['refer'], $watch_refers);
+		$watch_this_uri = $this->matchInList($target['uri'], $watch_uris);
 		
         if ($watch_this_ip || $watch_this_agent || $watch_this_isocode || $watch_this_refer || $watch_this_uri) {
             $dolog = true;
@@ -71,7 +66,7 @@ class Probe
         }
 
         if ($dolog) {
-            $item_id = DB::table('probe_log')->insertGetId($target);
+            $item_id = $this->setLogEntry($target);
         }
 
         if ($doban) {
@@ -79,7 +74,7 @@ class Probe
         }
     }
 
-    public static function matchInList($needle, $haystack)
+    public function matchInList($needle, $haystack)
     {
         $has_matches = false;
 
@@ -98,7 +93,7 @@ class Probe
         return $has_matches;
     }
 
-    public static function exactInList($needle, $haystack)
+    public function exactInList($needle, $haystack)
     {
         if (!is_array($haystack) && !empty($haystack)) {
             $haystack[0] = $haystack;
@@ -110,4 +105,112 @@ class Probe
             return false;
         }
     }
+	
+	public function setTarget() {
+        $target = [];
+        $target['ip'] = $this->target_ip;
+        $target['agent'] = $this->target_agent;
+        $target['identifier'] = $this->identifier;
+        $target['uri'] = $this->request_uri;
+        $target['refer'] = $this->request_refer;
+        $target['host'] = $this->request_host;
+        $target['city'] = $this->target_city;
+        $target['state'] = $this->target_state;
+        $target['postcode'] = $this->target_postal_code;
+        $target['hostname'] = $this->target_hostname;
+        $target['created_at'] = date('Y-m-d H:i:s');
+        $target['updated_at'] = date('Y-m-d H:i:s');
+        $target['iso_code'] = $this->target_iso_code;
+		
+		return $target;
+	}
+	
+	public function setLogEntry($target) {
+		$item_id = DB::table('probe_log')->insertGetId($target);;
+		return $item_id;
+	}
+	
+	public function getLogEntry() {
+		return $this->target_ip;
+	}
+	
+	public function setTargetIp() {
+		return (isset($_SERVER['REMOTE_ADDR'])) ? $_SERVER['REMOTE_ADDR'] : '127.0.0.1';
+	}
+	
+	public function getTargetIp() {
+		return $this->target_ip;
+	}
+	
+	public function setUserAgent() {
+		return (isset($_SERVER['HTTP_USER_AGENT'])) ? $_SERVER['HTTP_USER_AGENT'] : '';
+	}
+	
+	public function getUserAgent() {
+		return $this->target_agent;
+	}
+	
+	public function setIndentifier() {
+		return (!empty($identifier)) ? $identifier : '';
+	}
+	
+	public function getIndentifier() {
+		return $this->identifier;
+	}
+	
+	public function setRequestUri() {
+		return (isset($_SERVER['REQUEST_URI'])) ? $_SERVER['REQUEST_URI'] : '';
+	}
+	
+	public function getRequestUri() {
+		return $this->request_uri;
+	}
+	
+	public function setRequestRefer() {
+		return (isset($_SERVER['HTTP_REFERER'])) ? $_SERVER['HTTP_REFERER'] : '';
+	}
+	
+	public function getRequestRefer() {
+		return $this->request_refer;
+	}
+	
+	public function setRequestHost() {
+		return (isset($_SERVER['HTTP_HOST'])) ? $_SERVER['HTTP_HOST'] : '';
+	}
+	
+	public function getRequestHost() {
+		return $this->request_host;
+	}
+	
+	public function setTargetCity($target_location) {
+		return (isset($target_location->city) && !empty($target_location->city)) ? $target_location->city : '';
+	}
+	
+	public function getTargetCity() {
+		return $this->target_city;
+	}
+	
+	public function setTargetState($target_location) {
+		return (isset($target_location->state) && !empty($target_location->state)) ? $target_location->state : '';
+	}
+	
+	public function getTargetState() {
+		return $this->target_state;
+	}
+	
+	public function setTargetPostalCode($target_location) {
+		return (isset($target_location->postal_code) && !empty($target_location->postal_code)) ? $target_location->postal_code : '';
+	}
+	
+	public function getTargetPostalCode() {
+		return $this->target_postal_code;
+	}
+	
+	public function setTargetIsoCode($target_location) {
+		return (isset($target_location->iso_code) && !empty($target_location->iso_code)) ? $target_location->iso_code : '';
+	}
+	
+	public function getTargetIsoCode() {
+		return $this->target_iso_code;
+	}
 }
--- tests/ProbeTest.php
+++ tests/ProbeTest.php
@@ -4,20 +4,217 @@ namespace SeanHayes\Probe\Tests;
 
 use SeanHayes\Probe\Probe;
 
-class ProbeTest extends TestCase
+class ProbeTest extends \PHPUnit\Framework\TestCase
 {
     public function setUp(): void
     {
         parent::setUp();
+		
+		$this->probe = new Probe;
+		
+		//mock up config	
+	    $this->do_geolocate 	= false;
+	    $this->do_hostname 		= false;
+	    $this->ignore_ips 		= ['127.0.0.1'];
+	    $this->ignore_agents 	= ['Googlebot',
+		'Bingbot',
+		'Slurp',
+		'DuckDuckBot',
+		'Baiduspider',
+		'YandexBot',
+		'Sogou',
+		'Exabot',
+		'facebookexternalhit',
+		'facebot',
+		'ia_archiver'];
+	    $this->ignore_isocodes 	= [];
+	    $this->watch_ips 		= [];
+	    $this->watch_agents 	= ['Installatron',
+    	'80legs.com',
+		'Aboundex',
+		'BLEXBot',
+		'BlinkaCrawler',
+		'BomboraBot',
+		'BUbiNG',
+		'Bytespider',
+		'CCBot',
+		'CRAZYWEBCRAWLER',
+		'DotBot',
+		'EventMachine',
+		'istellabot',
+		'libwww-perl',
+		'LMAO',
+		'MauiBot',
+		'MJ12bot',
+		'NerdyBot',
+		'PhantomJS'];
+	    $this->watch_isocodes 	= [];
+	    $this->watch_refers 	= ['.cn',
+		'.ru',];
+		$this->watch_uris 		= ['/wp-login.php',
+		'/wp-admin.php',
+		'/xmlrpc.php',
+		'/wp-cron.php',
+		'/wp-content/',
+		'/wp-admin/',
+		'UNION%20SELECT%20',];
+		
+		$this->test_target = [
+			'ip' => '123.123.123.123',
+			'agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:7.0.1) Gecko/20100101 Firefox/7.0.1',
+			'identifier' => 'test',
+			'uri' => '/test',
+			'refer' => 'https://google.com/',
+			'host' => 'test.host',
+			'city' => 'San Diego',
+			'state' => 'CA',
+			'postcode' => '92121',
+			'hostname' => 'target.host',
+			'created_at' => '2020-01-01 00:00:01',
+			'updated_at' => '2020-01-01 00:00:02',
+			'iso_code' => 'US'
+		];
 
-        $this->assertCount(0, Probe::all());
+    }
+	
+    public function test_can_find_exact_string_in_list()
+    {
+		
+        $this->assertTrue($this->probe->exactInList('127.0.0.1', $this->ignore_ips));
+		
+    }
+	
+    public function test_can_find_exact_match_in_list()
+    {
+		
+        $this->assertTrue($this->probe->matchInList('Googlebot', $this->ignore_agents));
+		
+    }
+		
+    public function test_can_set_valid_target_ip()
+    {
+				
+		$this->assertRegExp('/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\z/', $this->probe->target_ip);
+				
+    }
+	
+    public function test_target_array_contains_ip()
+    {
+	    $this->assertArrayHasKey('ip', $this->probe->setTarget());
+    }
+	
+    public function test_target_array_contains_agent()
+    {
+	    $this->assertArrayHasKey('agent', $this->probe->setTarget());
+    }
+	
+    public function test_target_array_contains_identifier()
+    {
+	    $this->assertArrayHasKey('identifier', $this->probe->setTarget());
+    }
+	
+    public function test_target_array_contains_uri()
+    {
+	    $this->assertArrayHasKey('uri', $this->probe->setTarget());
+    }
+	
+    public function test_target_array_contains_refer()
+    {
+	    $this->assertArrayHasKey('refer', $this->probe->setTarget());
+    }
+	
+    public function test_target_array_contains_host()
+    {
+	    $this->assertArrayHasKey('host', $this->probe->setTarget());
+    }
+	
+    public function test_target_array_contains_city()
+    {
+	    $this->assertArrayHasKey('city', $this->probe->setTarget());
+    }
+	
+    public function test_target_array_contains_state()
+    {
+	    $this->assertArrayHasKey('state', $this->probe->setTarget());
+    }
+	
+    public function test_target_array_contains_postcode()
+    {
+	    $this->assertArrayHasKey('postcode', $this->probe->setTarget());
+    }
+	
+    public function test_target_array_contains_hostname()
+    {
+	    $this->assertArrayHasKey('hostname', $this->probe->setTarget());
+    }
+	
+    public function test_target_array_contains_created_at()
+    {
+	    $this->assertArrayHasKey('created_at', $this->probe->setTarget());
+    }
+	
+    public function test_target_array_contains_updated_at()
+    {
+	    $this->assertArrayHasKey('updated_at', $this->probe->setTarget());
+    }
+	
+    public function test_target_array_contains_iso_code()
+    {
+	    $this->assertArrayHasKey('iso_code', $this->probe->setTarget());
     }
 
-   /** @test */
-   public function it_can_create_a_probe_log()
-   {
-       $probe = Probe::findOrCreateFromString('string');
-
-       $this->assertCount(1, Probe::all());
-   }
+    public function test_can_set_valid_target_agent()
+    {
+		$this->assertIsString($this->probe->target_agent);				
+    }
+	
+    public function test_can_set_valid_identifier()
+    {
+		$this->assertIsString($this->probe->identifier);				
+    }
+	
+    public function test_can_set_valid_request_uri()
+    {
+		$this->assertIsString($this->probe->request_uri);				
+    }
+	
+    public function test_can_set_valid_request_refer()
+    {
+		$this->assertIsString($this->probe->request_refer);				
+    }
+	
+    public function test_can_set_valid_request_host()
+    {
+		$this->assertIsString($this->probe->request_host);				
+    }
+	
+    public function test_can_set_valid_target_location()
+    {
+		$this->assertIsString($this->probe->target_location);				
+    }
+	
+    public function test_can_set_valid_target_city()
+    {
+		$this->assertIsString($this->probe->target_city);				
+    }
+	
+    public function test_can_set_valid_target_state()
+    {
+		$this->assertIsString($this->probe->target_state);				
+    }
+	
+    public function test_can_set_valid_target_postal_code()
+    {
+		$this->assertIsString($this->probe->target_postal_code);				
+    }
+	
+    public function test_can_set_valid_target_hostname()
+    {
+		$this->assertIsString($this->probe->target_hostname);				
+    }
+	
+    public function test_can_set_valid_target_iso_code()
+    {
+		$this->assertIsString($this->probe->target_iso_code);				
+    }
 }
